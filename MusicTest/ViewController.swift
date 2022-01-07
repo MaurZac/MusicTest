4afbfeb0bea69b676a41dc5c6e6b8ed1fffa2143
Maur Zac <maur.mx@gmail.com>
2022-01-07 01:20:00 -0600

    Initial Commit

---
+++ MusicTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicTest
+//
+//  Created by MaurZac on 07/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
